98232fe97f2a9e5995fd1cfc926d65062c3b982b
Clinton Selke <clinuxrulz@gmail.com>
2026-03-15 08:25:14 +1000

    scroll fix on code mirror

--- src/components/CodeMirrorEditor.tsx
+++ src/components/CodeMirrorEditor.tsx
@@ -86,5 +86,5 @@ export default function Editor(props: EditorProps) {
     view?.destroy();
   });
 
-  return <div ref={editorParent} class="h-full w-full overflow-hidden" />;
+  return <div ref={editorParent} class="h-full w-full" />;
 }
