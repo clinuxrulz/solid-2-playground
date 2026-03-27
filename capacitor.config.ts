ebd9abb9eb60940449e878b376a0b72be0397175
Clinton Selke <clinuxrulz@gmail.com>
2026-03-27 18:18:36 +1000

    added compacitor-js for turning the PWA into an android apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.solid2.playground',
+  appName: 'Solid 2.0 Playground',
+  webDir: 'dist'
+};
+
+export default config;
